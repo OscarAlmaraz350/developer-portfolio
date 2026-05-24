5ba8f4ad91071981348a3f947542302cd8278a55
Oscar Almaraz <oscar.zainos@gmail.com>
2026-05-24 17:14:46 -0600

    changes on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2026 Oscar Almaraz</p>
+    <footer className="footer">
+      <div className="container footer-inner">
+        <div>
+          <p className="footer-name">© 2026 Oscar Almaraz</p>
+        </div>
+
+        <div className="footer-links">
+          <a href="#">GitHub</a>
+          <a href="#">LinkedIn</a>
+          <a href="#">Email</a>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
